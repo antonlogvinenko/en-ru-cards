913229372ac767a77dc4e08c455380b04d9591c1
Anton Logvinenko <logvinenko@farpost.com>
2013-06-30 23:49:34 +1100

    master Fixed scheduler

--- src/en_ru_cards/schedule.clj
+++ src/en_ru_cards/schedule.clj
@@ -9,23 +9,24 @@
   (let [word (random-word vocabulary storage)
         tweet (create-tweet-for-word word)]
     (if (nil? tweet)
-      (get-word-and-translation vocabulary storage)
+      (get-word-and-tweet vocabulary storage)
       [word tweet])))
 
 (defn tweet-new-card [dt {storage-file :storage-file vocabulary-file :vocabulary-file}]
   (let [storage (load-storage storage-file)
         vocabulary (read-vocabulary vocabulary-file)
         [word message] (get-word-and-tweet storage vocabulary)]
+    (println message)
     (store storage-file storage word)
     (tweet message)))
 
 (defn run [period vocabulary-file storage-file]
   (cj/defcronj hn
     :entries [{:id "en-ru-card"
-               :oprs {:storage-file storage-file
+               :opts {:storage-file storage-file
                       :vocabulary-file vocabulary-file}
                :handler tweet-new-card
-               :schedule (str "0 /" period " * * * * *")
+               :schedule (str "0 0 /" period " * * * *")
                }])
   (cj/start! hn))
 
